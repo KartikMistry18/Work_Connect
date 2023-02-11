a7ae8c3cfe8586183c515d24b28a2474ec00b5c3
Shantanu Gondane <113772055+shantanugondane@users.noreply.github.com>
2023-02-11 10:33:03 +0530

    updatedcustomer

--- src/components/Navbars.js
+++ src/components/Navbars.js
@@ -15,7 +15,7 @@ function Navbars() {
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="me-auto">
               <Nav.Link href="#features">Features</Nav.Link>
-              <Nav.Link href="#pricing">Pricing</Nav.Link>
+              <Nav.Link href="#pricing">Customer care</Nav.Link>
               <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.2">
---
+++ src/components/customercare.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./customercare.css"
+
+function customercare() {
+  return (
+    <div>
+        <h1>hello</h1>
+    </div>
+  )
+}
+
+export default customercare

4fc7dcf74b08772f1518de024902d95839e81750
Om Bhambale <115697133+Lazer29@users.noreply.github.com>
2023-02-12 07:56:59 +0530

    updated login signup

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -2,6 +2,7 @@ import { useRouter } from 'next/router'
 import React, { useEffect } from 'react'
 import { useAuth } from '../../context/AuthContext'
 
+
 const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
   const { user } = useAuth()
   const router = useRouter()

a85642f32af4964d93b5f6fa6fb4c540118053bf
KartikMistry18 <kartikmistry2003@gmail.com>
2023-02-12 10:53:39 +0530

    Updated faj.js

--- src/pages/faj.js
+++ src/pages/faj.js
@@ -45,99 +45,99 @@ function faj() {
           </p>
         </div>
         <div className={styles.element}>
-          <h1 className={styles.divHeading}>Plumber required</h1>
+          <h1 className={styles.divHeading}>AC technician required</h1>
           <p className={styles.divContent}>
-            replacement of old Tap and setting up the geyser
+            Servicing of the AC and fixing ofthe gaskit.
           </p>
           <p>
-            <strong>WAGES:</strong> 500 Rs <br />
-            <strong>LOCATION:</strong> 103 , Vasu building , JB nagar ,
+            <strong>WAGES:</strong> 4500 Rs <br />
+            <strong>LOCATION:</strong> 405 , Ayyappa Apartment , Ulhasnagar ,
+            Thane-E .<br />
+            <strong>CONTACT NUMBER:</strong> 7854127739
+          </p>
+        </div>
+        <div className={styles.element}>
+          <h1 className={styles.divHeading}>Janitor required</h1>
+          <p className={styles.divContent}>
+            Cleaning of Floor and classrooms in School.
+          </p>
+          <p>
+            <strong>WAGES:</strong> 7500 Rs <br />
+            <strong>LOCATION:</strong> Ryan International , JB nagar ,
+            Mulund-E .<br />
+            <strong>CONTACT NUMBER:</strong> 02289004538
+          </p>
+        </div>
+        <div className={styles.element}>
+          <h1 className={styles.divHeading}> Carpenter required</h1>
+          <p className={styles.divContent}>
+            Making of Sofa cum bed and polishing the furniture.
+          </p>
+          <p>
+            <strong>WAGES:</strong> 10000-20000 Rs <br />
+            <strong>LOCATION:</strong> 101 , Shilpoo building , JB nagar ,
             Andheri-E .<br />
-            <strong>CONTACT NUMBER:</strong> 9820056321
+            <strong>CONTACT NUMBER:</strong> 8983244187
+          </p>
+        </div>
+        <div className={styles.element}>
+          <h1 className={styles.divHeading}> Labour required</h1>
+          <p className={styles.divContent}>
+            At construction site of Viviana mall.
+          </p>
+          <p>
+            <strong>WAGES:</strong> 1500 Rs <br />
+            <strong>LOCATION:</strong> 505 , Terna building , Rajiv Road ,
+            Virar-E .<br />
+            <strong>CONTACT NUMBER:</strong> 02298787643
+          </p>
+        </div>
+        <div className={styles.element}>
+          <h1 className={styles.divHeading}>Maid required</h1>
+          <p className={styles.divContent}>
+            For cooking of food and doing all the household Work
+          </p>
+          <p>
+            <strong>WAGES:</strong> 7000 Rs <br />
+            <strong>LOCATION:</strong> 203 , Chetna building , Rawalnagar ,
+            Thane-E .<br />
+            <strong>CONTACT NUMBER:</strong> 8945743260
+          </p>
+        </div>
+        <div className={styles.element}>
+          <h1 className={styles.divHeading}>Painter required</h1>
+          <p className={styles.divContent}>
+            Painting of the house 2BHK.
+          </p>
+          <p>
+            <strong>WAGES:</strong> 20000-30000 Rs <br />
+            <strong>LOCATION:</strong> 604 , Neeta Apartments , Alok nagar ,
+            Ambernath-E .<br />
+            <strong>CONTACT NUMBER:</strong> 8765784324
+          </p>
+        </div>
+        <div className={styles.element}>
+          <h1 className={styles.divHeading}> Labour required</h1>
+          <p className={styles.divContent}>
+            Work at construction site of Runwal Developers.
+          </p>
+          <p>
+            <strong>WAGES:</strong> 2000 Rs <br />
+            <strong>LOCATION:</strong> Runwal Developers , Sahara building , Marol nagar ,
+            Andheri-E .<br />
+            <strong>CONTACT NUMBER:</strong> 9877875423
           </p>
         </div>
         <div className={styles.element}>
           <h1 className={styles.divHeading}>Plumber required</h1>
           <p className={styles.divContent}>
-            replacement of old Tap and setting up the geyser
+            Pipe Fitting and Water tank Cleaning.
           </p>
           <p>
-            <strong>WAGES:</strong> 500 Rs <br />
-            <strong>LOCATION:</strong> 103 , Vasu building , JB nagar ,
-            Andheri-E .<br />
-            <strong>CONTACT NUMBER:</strong> 9820056321
-          </p>
-        </div>
-        <div className={styles.element}>
-          <h1 className={styles.divHeading}>Plumber required</h1>
-          <p className={styles.divContent}>
-            replacement of old Tap and setting up the geyser
-          </p>
-          <p>
-            <strong>WAGES:</strong> 500 Rs <br />
-            <strong>LOCATION:</strong> 103 , Vasu building , JB nagar ,
-            Andheri-E .<br />
-            <strong>CONTACT NUMBER:</strong> 9820056321
-          </p>
-        </div>
-        <div className={styles.element}>
-          <h1 className={styles.divHeading}>Plumber required</h1>
-          <p className={styles.divContent}>
-            replacement of old Tap and setting up the geyser
-          </p>
-          <p>
-            <strong>WAGES:</strong> 500 Rs <br />
-            <strong>LOCATION:</strong> 103 , Vasu building , JB nagar ,
-            Andheri-E .<br />
-            <strong>CONTACT NUMBER:</strong> 9820056321
-          </p>
-        </div>
-        <div className={styles.element}>
-          <h1 className={styles.divHeading}>Plumber required</h1>
-          <p className={styles.divContent}>
-            replacement of old Tap and setting up the geyser
-          </p>
-          <p>
-            <strong>WAGES:</strong> 500 Rs <br />
-            <strong>LOCATION:</strong> 103 , Vasu building , JB nagar ,
-            Andheri-E .<br />
-            <strong>CONTACT NUMBER:</strong> 9820056321
-          </p>
-        </div>
-        <div className={styles.element}>
-          <h1 className={styles.divHeading}>Plumber required</h1>
-          <p className={styles.divContent}>
-            replacement of old Tap and setting up the geyser
-          </p>
-          <p>
-            <strong>WAGES:</strong> 500 Rs <br />
-            <strong>LOCATION:</strong> 103 , Vasu building , JB nagar ,
-            Andheri-E .<br />
-            <strong>CONTACT NUMBER:</strong> 9820056321
-          </p>
-        </div>
-        <div className={styles.element}>
-          <h1 className={styles.divHeading}>Plumber required</h1>
-          <p className={styles.divContent}>
-            replacement of old Tap and setting up the geyser
-          </p>
-          <p>
-            <strong>WAGES:</strong> 500 Rs <br />
-            <strong>LOCATION:</strong> 103 , Vasu building , JB nagar ,
-            Andheri-E .<br />
-            <strong>CONTACT NUMBER:</strong> 9820056321
-          </p>
-        </div>
-        <div className={styles.element}>
-          <h1 className={styles.divHeading}>Plumber required</h1>
-          <p className={styles.divContent}>
-            replacement of old Tap and setting up the geyser
-          </p>
-          <p>
-            <strong>WAGES:</strong> 500 Rs <br />
-            <strong>LOCATION:</strong> 103 , Vasu building , JB nagar ,
-            Andheri-E .<br />
-            <strong>CONTACT NUMBER:</strong> 9820056321
+            <strong>WAGES:</strong> 5000 Rs <br />
+            <strong>LOCATION:</strong> 303 , Kamla building , Brijwasi Road ,
+            Bandra-E .<br />
+            <strong>CONTACT NUMBER:</strong> 9875674423
           </p>
         </div>
       </div>

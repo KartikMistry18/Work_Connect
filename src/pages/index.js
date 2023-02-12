d6877900d24262c88935d625ed6028e2af89d17a
KrishayNair <krishay958@gmail.com>
2023-02-12 11:07:06 +0530

    index updated

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import Hero from "../components/Hero";
 import Works from "../components/Works";
 import Services from "../components/services";
 import Head from "next/head";
+import styles from "./index.module.css";
 export default function Home() {
   return (
     <div>
@@ -23,6 +24,15 @@ export default function Home() {
         agent-id="facd2057-66f2-4b77-a3b1-4ab31f250072"
         language-code="en"
       ></df-messenger> */}
+      <div className={styles.chatbot}>
+        <script src="https://www.gstatic.com/dialogflow-console/fast/messenger/bootstrap.js?v=1"></script>
+        <df-messenger
+          intent="WELCOME"
+          chat-title="WorkerToClientBot"
+          agent-id="5291fb65-78f7-477c-874a-0feea40a00f9"
+          language-code="en"
+        ></df-messenger>
+      </div>
     </div>
   );
 }

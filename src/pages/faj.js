3447caf3e93799c9d5ef0d33aea7f0df7b271552
KartikMistry18 <kartikmistry2003@gmail.com>
2023-02-12 09:05:26 +0530

    Updations done

--- src/pages/faj.js
+++ src/pages/faj.js
@@ -26,22 +26,22 @@ function faj() {
             To fix the tripped fuse and capacitor of fan .
           </p>
           <p>
-            <strong>WAGES:</strong> 500 Rs <br />
-            <strong>LOCATION:</strong> 103 , Vasu building , JB nagar ,
-            Andheri-E .<br />
-            <strong>CONTACT NUMBER:</strong> 9820056321
+            <strong>WAGES:</strong> 600 Rs <br />
+            <strong>LOCATION:</strong> 204 , Prerna building , DN nagar ,
+            Andheri-W .<br />
+            <strong>CONTACT NUMBER:</strong> 9987121453
           </p>
         </div>
         <div className={styles.element}>
-          <h1 className={styles.divHeading}>Plumber required</h1>
+          <h1 className={styles.divHeading}>Labour required</h1>
           <p className={styles.divContent}>
-            replacement of old Tap and setting up the geyser
+            At Construction site of the industrial area
           </p>
           <p>
-            <strong>WAGES:</strong> 500 Rs <br />
-            <strong>LOCATION:</strong> 103 , Vasu building , JB nagar ,
-            Andheri-E .<br />
-            <strong>CONTACT NUMBER:</strong> 9820056321
+            <strong>WAGES:</strong> 1000 Rs <br />
+            <strong>LOCATION:</strong> 602 , Kashyap building , Joshi nagar ,
+            Bhandup-E .<br />
+            <strong>CONTACT NUMBER:</strong> 9865547321
           </p>
         </div>
         <div className={styles.element}>
